cfcbe486826f3fd2042912a98088c13ac8f570a3
Yosuke Tanaka <yosket87@gmail.com>
2017-07-10 17:07:57 +0900

    トータルの表示

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User, Budget } from '../models';
 
+interface Total {
+  income: number;
+  outgo: number;
+}
+
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
@@ -14,21 +19,32 @@ export class ResultComponent implements OnInit {
     income: Budget[],
     outgo : Budget[]
   };
+  private total: Total;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       const data = JSON.parse(params.data);
-      this.users = data[0];
+      this.users = data[0].map(data => new User(data.name));
       this.budgets = { income: [], outgo: [] };
-      this.budgets.income = data[1];
-      this.budgets.outgo = data[2];
+      this.budgets.income = data[1].map(data => new Budget(data.title, data.price, data.user));
+      this.budgets.outgo = data[2].map(data => new Budget(data.title, data.price, data.user));
       this.calc();
     });
   }
 
   calc() {
+    this.total = this.sum(this.budgets);
+  }
 
+  sum(budgets: { income: Budget[], outgo: Budget[] }): Total {
+    const income = this.budgets.income
+      .map(i => i.price)
+      .reduce((p, c) => p + c);
+    const outgo = this.budgets.outgo
+      .map(i => i.price)
+      .reduce((p, c) => p + c);
+    return { income, outgo };
   }
 }
